79908cb06440c682265362729e5d89d86ca7fd83
GUENFOUDI MOHAMED <mohamed.guenfoudi@hotmail.com>
2022-07-30 21:11:58 +0100

    Update app_26.js

--- app_26.js
+++ app_26.js
@@ -1,3 +1,15 @@
 // app_25
 
-
+const checkTheme = document.querySelector('body #container .checkTheme .inpChange');
+checkTheme.onclick = function(){
+    const DarkMode = document.querySelector('body .DarkMode');
+    DarkMode.classList.toggle('ActiveDark');
+    if(DarkMode.classList.contains('ActiveDark')){
+        checkTheme.style.setProperty('--translateX','33px');    
+    }
+    else{
+        checkTheme.style.setProperty('--translateX','0px');
+    }
+    // const checkThemeOfBefore = window.getComputedStyle 'get property value css3'
+    
+}
