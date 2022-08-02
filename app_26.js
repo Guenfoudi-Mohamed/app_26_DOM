4d8e099ce93895e6d4baf2b6d049b8470bad2511
GUENFOUDI MOHAMED <mohamed.guenfoudi@hotmail.com>
2022-08-02 21:29:00 +0100

    Update app_26.js

--- app_26.js
+++ app_26.js
@@ -2,14 +2,20 @@
 
 const checkTheme = document.querySelector('body #container .checkTheme .inpChange');
 checkTheme.onclick = function(){
-    const DarkMode = document.querySelector('body .DarkMode');
+    const DarkMode = document.querySelector('body .DarkModeBlack');
     DarkMode.classList.toggle('ActiveDark');
     if(DarkMode.classList.contains('ActiveDark')){
-        checkTheme.style.setProperty('--translateX','33px');    
+        document.documentElement.style.setProperty('--translateX','33px');            
+        document.documentElement.style.setProperty('--backgroundCheckTheme','rgb(193, 127, 227)');            
+        document.documentElement.style.setProperty('--BeforeColor','#fff');            
+        document.documentElement.style.setProperty('--backGroundColor','rgb(193, 127, 227)');   
+        document.documentElement.style.setProperty('--colorBorder','#fff');   
     }
     else{
-        checkTheme.style.setProperty('--translateX','0px');
-    }
-    // const checkThemeOfBefore = window.getComputedStyle 'get property value css3'
-    
+        document.documentElement.style.setProperty('--translateX','0px');
+        document.documentElement.style.setProperty('--backgroundCheckTheme','rgb(231, 231, 231)');            
+        document.documentElement.style.setProperty('--BeforeColor','#000');
+        document.documentElement.style.setProperty('--backGroundColor','yellow');            
+        document.documentElement.style.setProperty('--colorBorder','#000');   
+    }    
 }
